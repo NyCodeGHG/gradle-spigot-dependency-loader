ca6d4c7adf76f580363fa3738754527f5021ada2
Michael Rittmeister <michael@rittmeister.in>
2022-01-30 01:58:33 +0000

    Fix issues with multiplatform - Support Kotlin MPP dependencies (Fix #4) - Bump dependencies

--- src/integrationTest/kotlin/TestData.kt
+++ src/integrationTest/kotlin/TestData.kt
@@ -19,3 +19,15 @@ val testGeneratedPluginYml = """
             |libraries:
             |- "com.squareup.okhttp3:okhttp:4.9.2"
             """.trimMargin()
+
+val testGeneratedPluginYmlMPP = """
+            |name: "ExamplePlugin"
+            |version: "1.0.0"
+            |main: "org.example.plugin.ExamplePlugin"
+            |commands:
+            |  "time":
+            |    description: "Displays the current time"
+            |    usage: "/time"
+            |libraries:
+            |- "org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.3.2"
+            """.trimMargin()
